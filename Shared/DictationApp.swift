800437f2449a4f83d8c18f91e854a3fdf61c41f3
sxc <qiuyequ@icloud.com>
2021-05-14 10:43:29 +0800

    Initial Commit

---
+++ Shared/DictationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictationApp.swift
+//  Shared
+//
+//  Created by Xiaochun Shen on 2021/5/14.
+//
+
+import SwiftUI
+
+@main
+struct DictationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
