c3a4eeb1e2bd83518f0a81b073bd850745ec1334
sxc <qiuyequ@icloud.com>
2021-05-18 14:25:39 +0800

    add modals view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -13,16 +13,31 @@ struct ContentView: View {
     @ObservedObject var audioRecorder: AudioRecorder
     @State var searchText = ""
     
+    @State private var presentingSheet = false
+    
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading)  {
-                
+//            VStack(alignment: .leading)  {
+            VStack {
                 SearchBar(searchText: $searchText)
                 
                 RecordingList(audioRecorder: audioRecorder)
                 
                 if audioRecorder.recording == false {
-                    Button(action: { self.audioRecorder.startRecording()}) {
+                    Button(action: {
+                            self.audioRecorder.startRecording()
+                        
+                            self.presentingSheet = true
+                        
+
+                        
+                    }
+
+                    
+                    ) {
+                        
+
+                        
                         Image(systemName: "circle.fill")
                             .resizable()
                             .aspectRatio(contentMode: .fill)
@@ -42,18 +57,67 @@ struct ContentView: View {
                             .padding(.bottom, 40)
                     }
                 }
+                
+            
+                
             }
             .navigationTitle("Voice recorder")
             .navigationBarItems(trailing: EditButton())
         }
+        
+        .sheet(isPresented: $presentingSheet) {
+            ModalView(title: "Recording",
+                      subtitle: "Presenting with Bool")
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView(audioRecorder: AudioRecorder())
+struct Sheet_PresentingWithBool: View {
+    @State private var presentingSheet = false
+    var body: some View {
+        Button("show modal") {
+            self.presentingSheet = true
+        }
+        .sheet(isPresented: $presentingSheet) {
+            ModalView(title: "Sheet",
+                      subtitle: "Presenting with Bool")
+        }
     }
 }
+
+struct ModalView: View {
+    @Environment(\.presentationMode) var presentation
+    let title: String
+    let subtitle: String
+    var body: some View {
+        VStack(spacing: 20) {
+            Text(title).font(.largeTitle)
+            Text(subtitle).font(.title).foregroundColor(.gray)
+//            Text("By change the State variable, you can trigger the Sheet to show")
+            
+            Spacer()
+            
+            Text("0:33:25").font(.largeTitle)
+            
+            Spacer()
+            Image(systemName: "stop.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 100, height: 100)
+                .clipped()
+                .foregroundColor(.red)
+                .padding(.bottom, 40)
+            
+            
+            Spacer()
+            Button("Dismiss") {
+                self.presentation.wrappedValue.dismiss()
+            }
+        }
+    }
+}
+
+
 
 struct SearchBar: View {
     @Binding var searchText: String
@@ -73,5 +137,12 @@ struct SearchBar: View {
         .frame(height: 40)
         .cornerRadius(13)
         .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(audioRecorder: AudioRecorder())
     }
 }
