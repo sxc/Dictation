e6dc856b540acee44c9e4da10620178374f640c4
sxc <qiuyequ@icloud.com>
2021-05-18 15:10:26 +0800

    remove some text

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-//            VStack(alignment: .leading)  {
+            //            VStack(alignment: .leading)  {
             VStack {
                 SearchBar(searchText: $searchText)
                 
@@ -25,18 +25,18 @@ struct ContentView: View {
                 
                 if audioRecorder.recording == false {
                     Button(action: {
-                            self.audioRecorder.startRecording()
+                        self.audioRecorder.startRecording()
                         
-                            self.presentingSheet = true
+                        self.presentingSheet = true
                         
-
+                        
                         
                     }
-
+                    
                     
                     ) {
                         
-
+                        
                         
                         Image(systemName: "circle.fill")
                             .resizable()
@@ -58,7 +58,7 @@ struct ContentView: View {
                     }
                 }
                 
-            
+                
                 
             }
             .navigationTitle("Voice recorder")
@@ -94,32 +94,30 @@ struct ModalView: View {
     var body: some View {
         VStack(spacing: 20) {
             Text(title).font(.largeTitle)
-//            Text(subtitle).font(.title).foregroundColor(.gray)
-//            Text("By change the State variable, you can trigger the Sheet to show")
             
             Spacer()
             
             Text("0:33:25").font(.largeTitle)
             
             ZStack {
-                        ForEach(0..<2) { i in
-                            Wave(strength: 50, frequency: 10, phase: self.phase)
-            
-                                .stroke(Color.blue, lineWidth: 3)
-                        }
-                    }
-                    .background(Color.white)
-                    .edgesIgnoringSafeArea(.all)
-                    .onAppear {
-                        withAnimation(Animation.linear(duration: 1).repeatForever(autoreverses: false)) {
-                            self.phase = .pi * 2
-                        }
-                    }
+                ForEach(0..<2) { i in
+                    Wave(strength: 50, frequency: 10, phase: self.phase)
+                        
+                        .stroke(Color.blue, lineWidth: 3)
+                }
+            }
+            .background(Color.white)
+            .edgesIgnoringSafeArea(.all)
+            .onAppear {
+                withAnimation(Animation.linear(duration: 1).repeatForever(autoreverses: false)) {
+                    self.phase = .pi * 2
+                }
+            }
             
             Spacer()
             Button(action:
                     {
-                
+                        
                         self.presentation.wrappedValue.dismiss()
                         
                         
@@ -132,7 +130,7 @@ struct ModalView: View {
                     .foregroundColor(.red)
                     .padding(.bottom, 40)
             }
-                
+            
             
             
             
@@ -172,55 +170,55 @@ struct Wave: Shape {
         get { phase }
         set { self.phase = newValue }
     }
-
+    
     // how high our waves should be
     var strength: Double
-
+    
     // how frequent our waves should be
     var frequency: Double
-
+    
     // how much to offset our waves horizontally
     var phase: Double
-
+    
     func path(in rect: CGRect) -> Path {
         let path = UIBezierPath()
-
+        
         // calculate some important values up front
         let width = Double(rect.width)
         let height = Double(rect.height)
         let midWidth = width / 2
         let midHeight = height / 2
         let oneOverMidWidth = 1 / midWidth
-
+        
         // split our total width up based on the frequency
         let wavelength = width / frequency
-
+        
         // start at the left center
         path.move(to: CGPoint(x: 0, y: midHeight))
-
+        
         // now count across individual horizontal points one by one
         for x in stride(from: 0, through: width, by: 1) {
             // find our current position relative to the wavelength
             let relativeX = x / wavelength
-
+            
             // find how far we are from the horizontal center
             let distanceFromMidWidth = x - midWidth
-
+            
             // bring that into the range of -1 to 1
             let normalDistance = oneOverMidWidth * distanceFromMidWidth
-
+            
             let parabola = -(normalDistance * normalDistance) + 1
-
+            
             // calculate the sine of that position, adding our phase offset
             let sine = sin(relativeX + phase)
-
+            
             // multiply that sine by our strength to determine final offset, then move it down to the middle of our view
             let y = parabola * strength * sine + midHeight
-
+            
             // add a line to here
             path.addLine(to: CGPoint(x: x, y: y))
         }
-
+        
         return Path(path.cgPath)
     }
 }
